fcba0b788a89f36e3c407da45c17393eaeec54f3
James Pond <james@cipher.host>
2024-06-18 18:46:38 -0300

    Add CSS and JS linters
    Signed-off-by: James Pond <james@cipher.host>

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  printWidth: 80,
+  tabWidth: 4,
+  useTabs: true,
+  singleQuote: true,
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  parenSpacing: true,
+  jsxBracketSameLine: true,
+  semi: true,
+  arrowParens: 'always',
+};
