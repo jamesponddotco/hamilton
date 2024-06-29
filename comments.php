faef193d7b5cd2af2daf2aaad45cc091793357a6
James Pond <james@cipher.host>
2024-06-29 18:40:08 -0300

    Fix linter issues with comments.php file
    Signed-off-by: James Pond <james@cipher.host>

--- comments.php
+++ comments.php
@@ -22,16 +22,20 @@ if ( post_password_required() ) {
 
 		<div class="comments">
 
-			<h3 class="comment-reply-title"><?php _e( 'Comments', 'hamilton' ) ?></h3>
+			<h3 class="comment-reply-title"><?php _e( 'Comments', 'hamilton' ); ?></h3>
 
 			<?php
 
-			wp_list_comments( array(
-				'style' 		=>	'div',
-				'avatar_size'	=>	110,
-			) );
+			wp_list_comments(
+				array(
+					'style'       => 'div',
+					'avatar_size' => 110,
+					'callback'    => 'hamilton_comment',
+				)
+			);
 
-			if ( paginate_comments_links( 'echo=0' ) ) : ?>
+			if ( paginate_comments_links( 'echo=0' ) ) :
+				?>
 
 				<div class="comments-pagination pagination"><?php paginate_comments_links(); ?></div>
 
