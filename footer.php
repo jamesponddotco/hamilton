c109af86889d7c96f76d1bedaf50e91c2390b7ce
James Pond <james@cipher.host>
2024-06-29 18:33:36 -0300

    Fix linter issues with footer.php file
    Signed-off-by: James Pond <james@cipher.host>

--- footer.php
+++ footer.php
@@ -12,12 +12,12 @@
 
 		<footer class="site-footer section-inner">
 
-			<p>&copy; <?php echo date( 'Y' ); ?> <a href="<?php echo esc_url( home_url() ); ?>" class="site-name"><?php bloginfo( 'name' ); ?></a></p>
+			<p>&copy; <?php echo esc_html( gmdate( 'Y' ) ); ?> <a href="<?php echo esc_url( home_url() ); ?>" class="site-name"><?php bloginfo( 'name' ); ?></a></p>
 			<p class="theme-by"><?php _e( 'Theme by', 'hamilton' ); ?> <a href="https://andersnoren.se">Anders Nor&eacute;n</a></p>
 
 		</footer><!-- footer -->
 
-	    <?php wp_footer(); ?>
+		<?php wp_footer(); ?>
 
 	</body>
 </html>
